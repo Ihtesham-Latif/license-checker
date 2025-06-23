8960dc79ecafeafa9e0359d1ca3d663ab045153f
Ihtesham Latif <111673637+Ihtesham-Latif@users.noreply.github.com>
2025-06-23 16:21:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = async function validateLicense() {
   try {
     const licenseKey = process.env.LICENSE_KEY;
     const response = await axios.post('https://license-api-two.vercel.app/validate', {
-      key: licenseKey,
-    });
+  key: process.env.LICENSE_KEY
+});
 
     if (!response.data.valid) {
       console.error('🚫 Invalid License Key. Exiting...');
